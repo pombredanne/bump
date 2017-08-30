4ccce0c6774f48c164313ca82baff99667e1c7ca
Dustin Ingram <di@users.noreply.github.com>
2017-08-30 18:32:19 -0500

    Some more tests

--- test.py
+++ test.py
@@ -33,6 +33,14 @@ class TestBump(unittest.TestCase):
         version = SemVer.parse("1.2.3-pre+build")
         self.check_version(version, 1, 2, 3, "pre", "build")
 
+    def test_str(self):
+        assert str(SemVer.parse("1")) == "1.0.0"
+        assert str(SemVer.parse("1.2")) == "1.2.0"
+        assert str(SemVer.parse("1.2.3")) == "1.2.3"
+        assert str(SemVer.parse("1.2.3-pre")) == "1.2.3-pre"
+        assert str(SemVer.parse("1.2.3+build")) == "1.2.3+build"
+        assert str(SemVer.parse("1.2.3-pre+build")) == "1.2.3-pre+build"
+
     def test_bump(self):
         version = SemVer(major=1, minor=2, patch=3)
         version.bump(major=True)
@@ -54,6 +62,14 @@ class TestBump(unittest.TestCase):
         version.bump(build="build")
         self.check_version(version, 1, 2, 3, None, "build")
 
+        version = SemVer(major=1, pre='pre')
+        version.bump()
+        self.check_version(version, 1, 0, 1, 'pre', None)
+
+        version = SemVer(major=1, build='build')
+        version.bump()
+        self.check_version(version, 1, 0, 1, None, "build")
+
     def test_cli(self):
         runner = CliRunner()  # noqa
 
@@ -64,6 +80,8 @@ class TestBump(unittest.TestCase):
         assert find_version("__version__='1.2.3'") == '1.2.3'
         assert find_version("    version='1.2.3',") == '1.2.3'
         assert find_version('    version="1.2.3",') == '1.2.3'
+        assert find_version('    version="1.2.3-dev",') == '1.2.3-dev'
+        assert find_version('    version="1.2.3+rc4",') == '1.2.3+rc4'
 
 
 if __name__ == '__main__':
