20be226a60d1bc323f9643d51fb3696195d18864
Dustin Ingram <di@users.noreply.github.com>
2018-02-27 14:43:58 -0600

    Metadata updates

--- setup.py
+++ setup.py
@@ -3,24 +3,24 @@ from setuptools import setup
 
 setup(
     name='bump',
-    version='1.0.0-dev',
+    version='1.0.0',
     description='Bumps package version numbers',
     # long_description=open('README.rst').read(),
     license='MIT',
-    url='https://github.com/marksteve/bump',
+    url='https://github.com/di/bump',
     author='Mark Steve Samson',
     author_email='hello@marksteve.com',
     classifiers=[
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.1",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords='bump increment package version',
     zip_safe=False,
     py_modules=['bump'],
-    install_requires=['click>=2,<3', 'first'],
+    install_requires=['click>=6,<7', 'first'],
     entry_points={
         'console_scripts': [
             'bump = bump:main'
