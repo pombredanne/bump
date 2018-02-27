0a59f335d2112d6b44a7e39b5ccf02030e41d71c
Dustin Ingram <di@users.noreply.github.com>
2018-02-27 14:44:10 -0600

    Linting

--- bump.py
+++ bump.py
@@ -16,22 +16,22 @@ class SemVer(object):
 
     def __repr__(self):
         # TODO: this is broken
-        return "<SemVer {}>".format(
-            ", ".join([
-                "{}={}".format(n, getattr(self, n))
+        return '<SemVer {}>'.format(
+            ', '.join([
+                '{}={}'.format(n, getattr(self, n))
                 for n in self.__slots__
             ])
         )
 
     def __str__(self):
-        version_string = ".".join(map(
+        version_string = '.'.join(map(
             str,
             [self.major, self.minor, self.patch]
         ))
         if self.pre:
-            version_string += "-" + self.pre
+            version_string += '-' + self.pre
         if self.build:
-            version_string += "+" + self.build
+            version_string += '+' + self.build
         return version_string
 
     @classmethod
@@ -84,22 +84,28 @@ def find_version(input_string):
 
 
 @click.command()
-@click.option('--major', '-M', 'major', flag_value=True,
-              help="Bump major number", default=False)
-@click.option('--minor', '-m', 'minor', flag_value=True,
-              help="Bump minor number", default=False)
-@click.option('--patch', '-p', 'patch', flag_value=True,
-              help="Bump patch number", default=True)
-@click.option('--pre', help="Set pre-release identifier")
-@click.option('--build', help="Set build metadata")
-@click.argument('input', type=click.File('rb'), default="setup.py")
-@click.argument('output', type=click.File('wb'), default="setup.py")
+@click.option(
+    '--major', '-M', 'major', flag_value=True, default=False,
+    help='Bump major number',
+)
+@click.option(
+    '--minor', '-m', 'minor', flag_value=True, default=False,
+    help='Bump minor number',
+)
+@click.option(
+    '--patch', '-p', 'patch', flag_value=True, default=True,
+    help='Bump patch number',
+)
+@click.option('--pre', help='Set pre-release identifier')
+@click.option('--build', help='Set build metadata')
+@click.argument('input', type=click.File('rb'), default='setup.py')
+@click.argument('output', type=click.File('wb'), default='setup.py')
 def main(input, output, **kwargs):
     contents = input.read().decode('utf-8')
     version_string = find_version(contents)
     version = SemVer.parse(version_string)
     version.bump(**kwargs)
-    new = pattern.sub(r"\g<1>{}\g<3>".format(version), contents)
+    new = pattern.sub('\g<1>{}\g<3>'.format(version), contents)
     output.write(new.encode())
     click.echo(version)
 
