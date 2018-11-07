144c5440431618a26db3031f8ffac276bbf7c23d
Dustin Ingram <di@users.noreply.github.com>
2018-11-07 11:31:10 -0600

    Support Python 3.7

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     keywords="bump increment package version",
     zip_safe=False,
